dadbbb1e5c8c0f33d63061cf9c25642f80010d0b
Valerio Maietta <valerio.maietta@gmail.com>
2024-12-13 12:47:44 +0100

    index suricata-* finalmente funzionante

--- suricata/start.sh
+++
@@ -1,9 +0,0 @@
-# ./suricata/start.sh
-#!/bin/bash
-
-# Configura l'interfaccia di rete
-ip link set eth0 up
-ip link set eth0 promisc on
-
-# Avvia Suricata con le opzioni corrette
-exec suricata -c /etc/suricata/suricata.yaml --af-packet $SURICATA_OPTIONS
